09e730ec9c7568ce4275a8b7b407a4229cd5f56b
Stefano Binando <sbinando@gmail.com>
2021-11-30 17:54:31 +0000

    E2E tests done in Cypress Calculator.js amended to return "Error" when dividing by zero

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -15,6 +15,35 @@ describe("Calculator", () => {
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '7')
   })
+  
+  it('should return correct result with multiple operations chained together', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number6').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '11')
+  })
 
+  it('should return the correct result for negative numbers', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-2')
+  })
+  
+  it('should return the "Error" result when dividing by zero', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'Error')
+  })
 
 })
