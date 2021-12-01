c9a710711bf20c21c746b33aefce62527557f67b
Stefano Binando <sbinando@gmail.com>
2021-12-01 09:36:33 +0000

    more E2E tests implemented

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,13 +7,45 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+  it('should have working number 7 buttons', () => {
+    cy.get('#number7').click();
+    cy.get('.display').should('contain', '7')
+  })
+  it('should have working number 6 buttons', () => {
+    cy.get('#number6').click();
+    cy.get('.display').should('contain', '6')
+  })
   
-  it('should return arithmetical result', () => {
+  it('should return result for addition', () => {
     cy.get('#number2').click();
     cy.get('#operator_add').click();
     cy.get('#number5').click();
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '7')
+  })
+
+  it('should return result for subtraction', () => {
+    cy.get('#number8').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '6')
+  })
+
+  it('should return result for multiplication', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '15')
+  })
+
+  it('should return result for division', () => {
+    cy.get('#number6').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3')
   })
   
   it('should return correct result with multiple operations chained together', () => {
@@ -28,6 +60,65 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('#operator-equals').click();
     cy.get('.display').should('contain', '11')
+  })
+
+  it('should return the correct result for positive numbers', () => {
+    cy.get('#number4').click();
+    cy.get('#operator_add').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '10')
+  })
+
+  it('should return the correct result for negative numbers', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number6').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-2')
+  })
+
+  it('should return the correct result for decimal numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '2.5')
+  })
+
+  it('should return the correct result for large numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '1e+27')
   })
 
   it('should return the correct result for negative numbers', () => {
